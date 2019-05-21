79f7972d48a6c4420d2c40936cb2c90781f57e2c
Anuraag Srivastava <as4378@nau.edu>
2019-05-20 22:05:02 -0700

    Added vignette section and modified R code

--- R/opart_gaussian.R
+++ R/opart_gaussian.R
@@ -1,11 +1,10 @@
-
 #' compute the optimal changepoint model for a vector of real-valued data
 #' and a non-negative real-valued penalty,
 #' given the square loss (to minimize) / gaussian likelihood (to maximize)
 #'
 #' @param n_data A number indicating the number of data points
 #' @param data_ptr A list of numbers for which the changepoint model is to be computed
-#' @param penalty A non-negative real number indicating penalty
+#' @param penalty A non-negative real number indicating penalty parameter
 #' @return A pointer to the optimal cost values and a pointer to the optimal segment ends
 #' @examples
 #' opart_gaussian(3, c(1,2,3), 1)
@@ -13,11 +12,15 @@
 
 
 opart_gaussian <- structure(function(n_data, data_ptr, penalty) {
-  result <- .C("opart_gaussian_interface", as.integer(n_data),
-               as.double(data_ptr), as.double(penalty),
-               cost_ptr = as.double(vector("double",length(data_ptr))),
-               end_ptr = as.integer(vector("integer",length(data_ptr))), error = integer(1),
+  result <- .C("opart_gaussian_interface",
+               as.integer(n_data),
+               as.double(data_ptr),
+               as.double(penalty),
+               cost_ptr = as.double(vector("double", n_data)),
+               end_ptr = as.integer(vector("integer", n_data)),
+               error = integer(1),
                PACKAGE="opart")
+
   error <- result$error
   cost <- result$cost_ptr
   ends <- result$end_ptr
--- src/opart_gaussian.c
+++ src/opart_gaussian.c
@@ -4,12 +4,10 @@
 int opart_gaussian(int n_data, double *data_ptr, double penalty, double *cost_ptr, int *end_ptr){
 	int i;
 
-	//cost_ptr = (double*)malloc(n_data*sizeof(double));
 	for(i = 0; i < n_data; i++){
 		cost_ptr[i] = data_ptr[i] / penalty;
 	}
 
-	//end_ptr = (int*)malloc(n_data*sizeof(int));
 	for(i = 0; i < n_data; i++){
 		end_ptr[i] = i;
 	}
