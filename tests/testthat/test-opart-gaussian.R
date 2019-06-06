6c0d30a42ca595fb0c7d2d1886df5d883498154e
Anuraag Srivastava <as4378@nau.edu>
2019-06-05 20:04:57 -0700

    added unit tests for 0 and large penalty

--- tests/testthat/test-opart-gaussian.R
+++ tests/testthat/test-opart-gaussian.R
@@ -13,6 +13,18 @@ test_that("opart gives error for negative penalty", {
 test_that("opart gives error when data points are less than 1", {
   expect_error({
     opart_gaussian(c(), 1)
-  }, "data vector must have atleast one element",
+  }, "the data vector must have more than 0 elements",
   fixed=TRUE)
+})
+
+#test for small penalty
+res <- opart_gaussian(c(1,2,3,4,5), 0.0001)
+test_that("all the data points are segment ends", {
+  expect_equal(res$end.vec, c(1,2,3,4,5))
+})
+
+#test for large penalty
+res <- opart_gaussian(c(1,2,3,4,5), 1000000)
+test_that("all the data points in one segment", {
+  expect_equal(res$end.vec, 5)
 })
